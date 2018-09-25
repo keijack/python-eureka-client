eb628909bde6f0ce2a73b5a2273ecf1f57065143
Keijack <keijack.wu@gmail.com>
2018-09-25 15:10:25 +0800

    update license to MIT

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 2.7",
-        "License :: OSI Approved :: Apache License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
