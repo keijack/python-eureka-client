674b2c80274e28c074bd56f6769dc3e124d23af5
Keijack <keijack.wu@gmail.com>
2019-01-18 15:45:44 +0800

    support eureka server url like

--- py_eureka_client/__urlopen_proxy__.py
+++ py_eureka_client/__urlopen_proxy__.py
@@ -16,6 +16,7 @@ _URL_REGEX = re.compile(
     r'^(?:http)s?://'  # http:// or https://
     r'(([A-Z0-9_~!.%]+):([A-Z0-9_~!.%]+)@)?' # basic authentication -> username:password@
     r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+    r'(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?)|' # domain name without `.`
     r'localhost|'  # localhost...
     r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
     r'(?::\d+)?'  # optional port
