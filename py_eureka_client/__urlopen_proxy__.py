1c12b39346639c6ca4d009382b68b9f1e2941ce5
Keijack <keijack.wu@gmail.com>
2019-03-08 15:18:40 +0800

    support gzip encoding

--- py_eureka_client/__urlopen_proxy__.py
+++ py_eureka_client/__urlopen_proxy__.py
@@ -2,6 +2,7 @@
 import socket
 import re
 import base64
+import gzip
 
 try:
     import urllib.request as urllib2
@@ -11,12 +12,13 @@ except ImportError:
     import urllib2
     from urllib2 import HTTPError
     from urllib2 import URLError
+    from StringIO import StringIO
 
 _URL_REGEX = re.compile(
     r'^(?:http)s?://'  # http:// or https://
-    r'(([A-Z0-9_~!.%]+):([A-Z0-9_~!.%]+)@)?' # basic authentication -> username:password@
+    r'(([A-Z0-9_~!.%]+):([A-Z0-9_~!.%]+)@)?'  # basic authentication -> username:password@
     r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
-    r'(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?)|' # domain name without `.`
+    r'(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?)|'  # domain name without `.`
     r'localhost|'  # localhost...
     r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
     r'(?::\d+)?'  # optional port
@@ -52,11 +54,11 @@ class Request(urllib2.Request, object):
         url_auth = url_obj[1]
         try:
             super(Request, self).__init__(url_addr, data=data, headers=headers,
-                                      origin_req_host=origin_req_host, unverifiable=unverifiable,
-                                      method=method)
+                                          origin_req_host=origin_req_host, unverifiable=unverifiable,
+                                          method=method)
         except TypeError:
             super(Request, self).__init__(url_addr, data=data, headers=headers,
-                                      origin_req_host=origin_req_host, unverifiable=unverifiable)
+                                          origin_req_host=origin_req_host, unverifiable=unverifiable)
             self.get_method = lambda: method if method is not None else "GET"
         if url_auth is not None:
             self.add_header('Authorization', 'Basic %s' % url_auth)
@@ -65,11 +67,22 @@ class Request(urllib2.Request, object):
 def urlopen(url, data=None, timeout=socket._GLOBAL_DEFAULT_TIMEOUT,
             cafile=None, capath=None, cadefault=False, context=None):
     if isinstance(url, urllib2.Request):
-        return urllib2.urlopen(url, data=data, timeout=timeout,
-                               cafile=cafile, capath=capath, cadefault=cadefault, context=context)
+        request = url
     elif isinstance(url, str):
         request = Request(url)
-        return urllib2.urlopen(request, data=data, timeout=timeout,
-                               cafile=cafile, capath=capath, cadefault=cadefault, context=context)
     else:
         raise URLError("Unvalid URL")
+    request.add_header("Accept-encoding", "gzip")
+    res = urllib2.urlopen(request, data=data, timeout=timeout,
+                          cafile=cafile, capath=capath, cadefault=cadefault, context=context)
+
+    if res.info().get("Content-Encoding") == "gzip":
+        try:
+            # python2
+            f = gzip.GzipFile(fileobj=StringIO(res.read()))
+        except NameError:
+            f = gzip.GzipFile(fileobj=res)
+    else:
+        f = res
+
+    return f
