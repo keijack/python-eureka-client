b7babc8f6f88f67983734f06945014a0aac647ef
Keijack <keijack.wu@gmail.com>
2020-09-26 09:16:11 +0800

    refactor logger, the lineno now will output correctly.

--- py_eureka_client/logger.py
+++ py_eureka_client/logger.py
@@ -43,45 +43,25 @@ _handler.setLevel(_LOG_LEVEL_)
 _msg_cache = queue.Queue(10000)
 
 
-class CachingLogger(object):
+class CachingLogger(logging.Logger):
 
-    def __init__(self, name):
-        self.name = name
+    def _call_handlers(self, record):
+        super(CachingLogger, self).callHandlers(record)
 
-    def _log(self, mth, msg, *args, **kwargs):
-        _msg_cache.put({
-            "name": self.name,
-            "method": mth,
-            "msg": msg,
-            "args": args,
-            "kwargs": kwargs,
-        })
-
-    def debug(self, msg, *args, **kwargs):
-        self._log("debug", msg, *args, **kwargs)
-
-    def info(self, msg, *args, **kwargs):
-        self._log("info", msg, *args, **kwargs)
-
-    def warning(self, msg, *args, **kwargs):
-        self._log("warn", msg, *args, **kwargs)
-
-    def warn(self, msg, *args, **kwargs):
-        self._log("warn", msg, *args, **kwargs)
-
-    def error(self, msg, *args, **kwargs):
-        self._log("error", msg, *args, **kwargs)
-
-    def exception(self, msg, *args, **kwargs):
-        self.error(msg, *args, exc_info=sys.exc_info(), ** kwargs)
+    def callHandlers(self, record):
+        _msg_cache.put((self, record))
 
 
 def set_level(level):
+    global _LOG_LEVEL_
     lv = level.upper()
     if lv in ("DEBUG", "INFO", "WARN", "ERROR"):
         _handler.setLevel(lv)
         _logger_ = get_logger("Logger")
         _logger_.info("global logger set to %s" % lv)
+        _LOG_LEVEL_ = lv
+        for l in __cache_loggers.values():
+            l.setLevel(lv)
 
 
 def set_handler(handler):
@@ -91,41 +71,18 @@ def set_handler(handler):
 
 def get_logger(tag="py-eureka-client"):
     if tag not in __cache_loggers:
-        __cache_loggers[tag] = CachingLogger(tag)
+        __cache_loggers[tag] = CachingLogger(tag, _LOG_LEVEL_)
+        __cache_loggers[tag].addHandler(_handler)
     return __cache_loggers[tag]
 
-
-def _get_real_logger(tag=""):
-    # type (str) -> logging.Logger
-    if tag in __loggers:
-        return __loggers[tag]
-
-    logger = logging.getLogger(tag)
-    logger.addHandler(_handler)
-    logger.setLevel(_handler.level)
-    __loggers[tag] = logger
-    return logger
-
-
-def _log_():
-    while(True):
+def _log_msg_from_queue():
+    while True:
         msg = _msg_cache.get()
-        logger = _get_real_logger(msg["name"])
-        mth = msg["method"]
-        if mth == "debug":
-            logger.debug(msg["msg"], *msg["args"], **msg["kwargs"])
-        elif mth == "info":
-            logger.info(msg["msg"], *msg["args"], **msg["kwargs"])
-        elif mth == "warn":
-            logger.warn(msg["msg"], *msg["args"], **msg["kwargs"])
-        elif mth == "error":
-            logger.error(msg["msg"], *msg["args"], **msg["kwargs"])
+        msg[0]._call_handlers(msg[1])
 
-
-def _run_log_thread():
-    log_thread = Thread(target=_log_, name="log_thread")
+def _log_msg_in_backgrond():
+    log_thread = Thread(target=_log_msg_from_queue, name="logging-thread")
     log_thread.daemon = True
     log_thread.start()
 
-
-_run_log_thread()
+_log_msg_in_backgrond()
