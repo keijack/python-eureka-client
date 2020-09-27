41ab95874e60b6ba83a2cde5fa90e264d8c8cd47
Keijack <keijack.wu@gmail.com>
2020-09-27 19:19:19 +0800

    update test cases

--- tests/py_eureka_client/EurekaServerParseTest.py
+++ tests/py_eureka_client/EurekaServerParseTest.py
@@ -25,7 +25,7 @@ SOFTWARE.
 import unittest
 
 import py_eureka_client.logger as logger
-from py_eureka_client.eureka_client import EurekaServer
+from py_eureka_client.eureka_client import EurekaServerConf
 
 
 class TestEurekaServer(unittest.TestCase):
@@ -35,15 +35,15 @@ class TestEurekaServer(unittest.TestCase):
         logger.set_level("DEBUG")
 
     def test_init_eureka_server(self):
-        es = EurekaServer(eureka_servers="https://a@10.0.2.16:8761/eureka,https://10.0.2.16:8762",
+        es = EurekaServerConf(eureka_server="https://a@10.0.2.16:8761/eureka,https://10.0.2.16:8762",
                           eureka_basic_auth_user="keijack", eureka_basic_auth_password="!@#qwe", zone="zone1")
         print(es.servers)
 
     def test_init_eureka_dns(self):
-        es = EurekaServer(eureka_domain="keijack.com", eureka_basic_auth_user="keijack",
+        es = EurekaServerConf(eureka_domain="keijack.com", eureka_basic_auth_user="keijack",
                           eureka_basic_auth_password="!@#qwe", region="dev", zone="zone1")
         print(es.servers_not_in_zone)
 
     def test_init_availability_zones(self):
-        es = EurekaServer(eureka_availability_zones={"zone1": ["https://myec2.com", "myec1.com"], "zone2": "myzone2.com, myzone22.com"})
+        es = EurekaServerConf(eureka_availability_zones={"zone1": ["https://myec2.com", "myec1.com"], "zone2": "myzone2.com, myzone22.com"})
         print(es.servers)
