42511051b2037a69075047dbfb6539107cec669e
Keijack <keijack.wu@gmail.com>
2020-10-19 09:41:55 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 import setuptools
+import os
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+ver = os.environ["py_eureka_client_version"]
+if not ver:
+    exit(1)
+
+print("releasing version %s" % ver)
+
 setuptools.setup(
     name="py_eureka_client",
-    version="0.8.4",
+    version=ver,
     author="Keijack",
     author_email="keijack.wu@gmail.com",
     description="A eureka client written in python. Support registering your python component to Eureka Server, as well as calling remote services by pulling the the Eureka registry. ",
@@ -19,6 +26,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
